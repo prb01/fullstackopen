158054fe8cecd63242e8e305dce316eeaa997546
prb01 <p.bergstroem@gmail.com>
2022-02-28 18:57:19 -0500

    1.4/1.5: Parts array & course name abstracted to single object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,7 +13,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  var rows = [];
+  let rows = [];
   
   props.parts.forEach(el => {
     rows.push( <Part part={el}/> );
@@ -23,31 +23,43 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  let sum = 0;
+
+  props.parts.forEach(el => {
+    sum += el.exercises;
+  });
+
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {sum}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = 
+  {
+    name: 'Half Stack application development',
+    parts: 
+    [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
