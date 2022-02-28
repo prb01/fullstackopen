9813398a5390a3ef6b90987352f5ecdc822c1bdb
prb01 <p.bergstroem@gmail.com>
2022-02-28 18:44:49 -0500

    1.3 objects added. Code refactored.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,18 +7,19 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
 
 const Content = (props) => {
   var rows = [];
-  for (let i = 0; i < props.part.length; i++) {
-    rows.push(<Part part={props.part[i]} exercises={props.exercises[i]} />);
-  }
   
-  return (<div>{rows}</div>)
+  props.parts.forEach(el => {
+    rows.push( <Part part={el}/> );
+  });
+
+  return (<div>{rows}</div>);
 }
 
 const Total = (props) => {
@@ -29,18 +30,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
