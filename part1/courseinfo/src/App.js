83ee36685edb02eb376f218d582e1ee2047efa6f
prb01 <p.bergstroem@gmail.com>
2022-02-28 16:43:07 -0500

    exercise 1.2 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,12 +4,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
     </p>
   )
+}
+
+const Content = (props) => {
+  var rows = [];
+  for (let i = 0; i < props.part.length; i++) {
+    rows.push(<Part part={props.part[i]} exercises={props.exercises[i]} />);
+  }
+  
+  return (<div>{rows}</div>)
 }
 
 const Total = (props) => {
@@ -20,7 +29,6 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -31,9 +39,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
