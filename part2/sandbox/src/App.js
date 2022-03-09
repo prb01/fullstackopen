177be16220b26aefc59a58d47dac3188e7618e15
prb01 <p.bergstroem@gmail.com>
2022-03-09 11:54:04 -0500

    Sandbox changes due to exercises in 2b

--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -1,14 +1,47 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = e => {
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes([...notes, noteObject])
+    setNewNote('')
+  }
+
+  const handleNoteChange = e => {
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll ?
+    notes : notes.filter( note => note.important )
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+          <input value={newNote} onChange={handleNoteChange}/>
+          <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
