fcf0a5b527017e35aa75da26a538c1286819f386
prb01 <p.bergstroem@gmail.com>
2022-03-09 12:44:46 -0500

    Phonebook step4 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,13 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '001-300-5467' }
+    { name: 'Arto Hellas', number: '001-300-5467' },
+    { name: 'Caro Duque', number: '+54-001-4567' },
+    { name: 'Denny Maliakla', number: '+41 41 750 55 78' }
   ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addNumber = e => {
     e.preventDefault()
@@ -23,16 +26,31 @@ const App = () => {
     setNewNum('')
   }
 
+  const personsToShow = () => {
+    if (newSearch === '') {
+      return persons
+    }
+
+    return persons.filter(person => 
+      person.name.toLowerCase().includes(newSearch.toLowerCase())
+      )
+  }
+
   const handleNameChange = e => {
     setNewName(e.target.value)
   }
   const handleNumChange = e => {
     setNewNum(e.target.value)
   }
+  const handleSearchChange = e => {
+    setNewSearch(e.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <input value={newSearch} onChange={handleSearchChange}/>
+      <h3>Add New</h3>
       <form onSubmit={addNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -45,7 +63,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={personsToShow()} />
     </div>
   )
 }
