b44a2160b0249f03f5512b2be42dbf66a3e99ce9
prb01 <p.bergstroem@gmail.com>
2022-03-11 12:15:26 -0500

    End of part 2c: getting data from server

--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -1,10 +1,23 @@
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(res => {
+        console.log('promise fulfilled')
+        setNotes(res.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render',notes.length,'notes')
 
   const addNote = e => {
     e.preventDefault()
