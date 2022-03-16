917bd2a2157103b65f54568ca11d2124f685b9f6
prb01 <p.bergstroem@gmail.com>
2022-03-16 11:14:48 -0500

    3.3: Phonebook step3 (get person)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -27,11 +27,8 @@ let persons = [
   },
 ]
 
-app.get('/api/persons', (req,res) => {
-  res.json(persons)
-})
-
-app.get('/info', (req, res) => {
+//GET INFO
+app.get("/info", (req, res) => {
   const now = new Date()
   const len = persons.length
 
@@ -39,6 +36,24 @@ app.get('/info', (req, res) => {
     `<p>Phonebook has info for ${len} people.</p>
     <p>${now}</p>`
   )
+})
+
+//GET PERSONS
+app.get("/api/persons", (req, res) => {
+  res.json(persons)
+})
+
+//GET PERSONS/ID
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((p) => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.statusMessage = "Person does not exist"
+    res.status(404).end()
+  }
 })
 
 app.listen(PORT, () => {
