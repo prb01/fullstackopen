83b4b643d43b74d86f0601540afeee13ed28d1a5
prb01 <p.bergstroem@gmail.com>
2022-03-17 12:34:20 -0500

    uibuild

--- part2/notes-front/src/services/notes.js
+++ part2/notes-front/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = "https://warm-springs-42874.herokuapp.com/api/notes"
+const baseUrl = "/api/notes"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
