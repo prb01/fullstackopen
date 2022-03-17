d32808fadb968d3ea45bf402741ac3da059eb37b
prb01 <p.bergstroem@gmail.com>
2022-03-17 09:16:48 -0500

    notes frontend backend connected

--- part2/sandbox/src/services/notes.js
+++ part2/sandbox/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = "https://warm-springs-42874.herokuapp.com/api/notes"
 
 const getAll = () => {
   const request = axios.get(baseUrl)
--- part3/sandbox/index.js
+++ part3/sandbox/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
+app.use(express.static("build"))
 app.use(express.json())
-
+app.use(cors())
 
 const requestLogger = (req, res, next) => {
   console.log("Method:", req.method)
@@ -90,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
