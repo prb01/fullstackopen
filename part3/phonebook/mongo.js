146bcd35947ac33fe44d37703e32ec8f5f70dfbb
prb01 <p.bergstroem@gmail.com>
2022-03-18 12:15:02 -0500

    3.12: Phonebook CLI db (mongo)

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 if (process.argv.length < 3) {
   console.log(
@@ -9,3 +9,30 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 const url = `mongodb+srv://fullstack:${password}@cluster0.ncd46.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model("Person", personSchema)
+
+//CONNECT to MONGODB
+mongoose.connect(url)
+
+//ADD NEW PERSON IF ARGS PROVIDED
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({ name, number })
+
+  person.save().then((res) => {
+    console.log(`added ${name}'s number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('phonebook:')
+  Person.find({}).then(res => {
+    res.forEach(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+}
