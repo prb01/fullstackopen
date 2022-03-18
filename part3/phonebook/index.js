1fd798c833f8d730791ef045400432f16c4f39b1
prb01 <p.bergstroem@gmail.com>
2022-03-18 15:19:27 -0500

    3.15: Phonebook db step3 (delete)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -44,10 +44,10 @@ app.get("/api/persons/:id", (req, res) => {
 
 //DELETE PERSONS/ID
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter((p) => p.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndDelete(req.params.id)
+  .then(result => {
+    res.status(204).end()
+  })
 })
 
 //POST PERSONS
