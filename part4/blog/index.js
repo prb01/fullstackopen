57c94eec06f131b439871aa22de29df660bd7193
prb01 <p.bergstroem@gmail.com>
2022-03-21 11:22:57 -0500

    4.2: Blog list step2 (refactored app)

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,16 +1,5 @@
-// const http = require("http")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const blogsRouter = require("./controllers/blogs")
 const config = require("./utils/config")
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-app.use("/api/blogs", blogsRouter)
+const app = require("./app")
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
