dfa378752e0ece8f1d1fbefe8f71b37a2a383adc
prb01 <p.bergstroem@gmail.com>
2022-03-21 10:49:33 -0500

    4.1 Blog list step1 (initialize node)

---
+++ part4/blog/index.js
@@ -0,0 +1,41 @@
+const http = require("http")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model("Blog", blogSchema)
+const password = process.env.PASS
+
+const mongoUrl =
+  `mongodb+srv://fullstack:${password}@cluster0.ncd46.mongodb.net/blogApp?retryWrites=true&w=majority`
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
