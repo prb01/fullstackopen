78d9b568600a65f61dd0f7918dfc2bf3fb61d105
prb01 <p.bergstroem@gmail.com>
2022-03-21 10:54:11 -0500

    refactor(models): blog model separated into module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -3,15 +3,8 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
+const Blog = require("./models/blog")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
 const password = process.env.PASS
 
 const mongoUrl =
