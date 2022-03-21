b3f2c04288540aa8d6042c41651a9d689c95c03c
prb01 <p.bergstroem@gmail.com>
2022-03-21 11:10:28 -0500

    refactor(controller): blogsRouter separated into module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,34 +1,17 @@
-const http = require("http")
+// const http = require("http")
 const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
-const Blog = require("./models/blog")
+const blogsRouter = require("./controllers/blogs")
+const config = require("./utils/config")
 
-const password = process.env.PASS
-
-const mongoUrl =
-  `mongodb+srv://fullstack:${password}@cluster0.ncd46.mongodb.net/blogApp?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/blogs", blogsRouter)
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
