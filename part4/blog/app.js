c67207b2dfbcff20e38dd36e4949588d6bbfe0b4
prb01 <p.bergstroem@gmail.com>
2022-03-30 16:39:27 -0500

    4.18: blogs expansion step6 (token auth)

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,7 +3,8 @@ const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
-const userssRouter = require("./controllers/users")
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 const config = require("./utils/config")
 require("express-async-errors")
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
-app.use("/api/users", userssRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
 module.exports = app
