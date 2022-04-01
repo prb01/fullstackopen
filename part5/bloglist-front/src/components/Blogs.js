272fcdaba572214bc346527f82ad5c469a672a33
prb01 <p.bergstroem@gmail.com>
2022-04-01 14:30:06 -0500

    5.7: blogs front step7 (toggle full blog details)

--- part5/bloglist-front/src/components/Blogs.js
+++ part5/bloglist-front/src/components/Blogs.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Blogs = ({ blogs }) => (
   <div>
     <ul>
@@ -8,10 +10,37 @@ const Blogs = ({ blogs }) => (
   </div>
 )
 
-const Blog = ({ blog }) => (
-  <li>
-    <strong>{blog.title}</strong> by {blog.author}
-  </li>
-)
+const Blog = ({ blog }) => {
+  const [fullView, setFullView] = useState(false)
+
+  const showWhenSmallView = { display: fullView ? "none" : "" }
+  const showWhenFullView = { display: fullView ? "" : "none" }
+
+  const toggleView = () => {
+    setFullView(!fullView)
+  }
+
+  return (
+    <>
+      <li style={showWhenSmallView}>
+        <strong>{blog.title}</strong> by {blog.author}
+        <button onClick={toggleView}>view</button>
+      </li>
+
+      <li style={showWhenFullView}>
+        <strong>{blog.title}</strong> by {blog.author}
+        <button onClick={toggleView}>hide</button>
+        <ul>
+          <li>url: {blog.url}</li>
+          <li>
+            likes: {blog.likes}
+            <button onClick={() => console.log("like++")}>like</button>
+          </li>
+          <li>user: {blog.user?.name || "user unknown"}</li>
+        </ul>
+      </li>
+    </>
+  )
+}
 
 export default Blogs
