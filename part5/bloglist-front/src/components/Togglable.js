cec33656dc9262ca690735d2a944d014a0137a43
prb01 <p.bergstroem@gmail.com>
2022-04-02 11:03:59 -0500

    5.11: blogs front step11 (PropTypes)

--- part5/bloglist-front/src/components/Togglable.js
+++ part5/bloglist-front/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import { useState, useImperativeHandle, forwardRef } from "react"
+import PropTypes from "prop-types"
 
 const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
@@ -27,5 +28,9 @@ const Togglable = forwardRef((props, ref) => {
     </div>
   )
 })
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
