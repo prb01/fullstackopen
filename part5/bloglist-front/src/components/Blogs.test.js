572e0f7b6802091ff2afbf7407241e864a288ba6
prb01 <p.bergstroem@gmail.com>
2022-04-03 10:28:22 -0500

    5.14: blogs front tests step2 (expanded)

--- part5/bloglist-front/src/components/Blogs.test.js
+++ part5/bloglist-front/src/components/Blogs.test.js
@@ -1,18 +1,25 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Blogs from "./Blogs"
 
-describe("<Blogs/>", () => {
-  test("renders only title & author on initialization", () => {
-    const blogs = [{
-      title: "Blog title",
-      author: "Bloggy",
-      url: "http://www.blog.com",
-      likes: 9999,
-      id: "dsf987"
-    }]
+describe("<Blog/>", () => {
+  let blogs
 
+  beforeEach(() => {
+    blogs = [
+      {
+        title: "Blog title",
+        author: "Bloggy",
+        url: "http://www.blog.com",
+        likes: 9999,
+        id: "dsf987",
+      },
+    ]
+  })
+
+  test("renders only title & author on initialization", () => {
     const { container } = render(<Blogs blogs={blogs} />)
 
     const collapsed = container.querySelector(".collapsed")
@@ -22,7 +29,26 @@ describe("<Blogs/>", () => {
     expect(collapsed).toHaveTextContent("Bloggy")
     expect(collapsed).not.toHaveTextContent("http://www.blog.com")
     expect(collapsed).not.toHaveTextContent("9999")
-    
+
+    expect(collapsed).not.toHaveStyle("display: none")
     expect(expanded).toHaveStyle("display: none")
+  })
+
+  test("renders title, author, url, likes on expand", async () => {
+    const { container } = render(<Blogs blogs={blogs} />)
+
+    const collapsed = container.querySelector(".collapsed")
+    const expanded = container.querySelector(".expanded")
+    const viewButton = screen.getByText("view")
+
+    await userEvent.click(viewButton)
+
+    expect(expanded).toHaveTextContent("Blog title")
+    expect(expanded).toHaveTextContent("Bloggy")
+    expect(expanded).toHaveTextContent("http://www.blog.com")
+    expect(expanded).toHaveTextContent("9999")
+
+    expect(collapsed).toHaveStyle("display: none")
+    expect(expanded).not.toHaveStyle("display: none")
   })
 })
