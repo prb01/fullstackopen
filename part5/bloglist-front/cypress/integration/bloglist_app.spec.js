024eea7f00716a8234d6d70ac80246f1284c8523
prb01 <p.bergstroem@gmail.com>
2022-04-14 16:44:09 -0500

    5.18: blogs e2e tests, step 2 (login)

--- part5/bloglist-front/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-front/cypress/integration/bloglist_app.spec.js
@@ -1,11 +1,37 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset")
+    cy.request("POST", "http://localhost:3003/api/users", {
+      name: "test password",
+      username: "test",
+      password: "password",
+    })
     cy.visit("http://localhost:3000")
   })
 
   it("login form is shown", function () {
     cy.contains("Username")
     cy.contains("Password")
+  })
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("test")
+      cy.get("#password").type("password")
+      cy.contains("Login").click()
+
+      cy.contains("logged in")
+      cy.contains("logout")
+      cy.get(".info").should("have.css", "color", "rgb(47, 157, 98)")
+    })
+
+    it("fails with incorrect credentials", function () {
+      cy.get("#username").type("test")
+      cy.get("#password").type("wrong")
+      cy.contains("Login").click()
+
+      cy.get("html").should("not.contain", "logged in")
+      cy.get(".error").should("have.css", "color", "rgb(186, 59, 69)")
+    })
   })
 })
