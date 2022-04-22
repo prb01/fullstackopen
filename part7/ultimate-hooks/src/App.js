6722094e45c67545f55c0bf463debe4710194947
prb01 <p.bergstroem@gmail.com>
2022-04-22 14:37:47 -0500

    7.8: ultimate hooks (useResource)

---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const useField = (type) => {
+  const [value, setValue] = useState("")
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue("")
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset
+  }
+}
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => setResources(response.data))
+  },[])
+
+  const create = (resource) => {
+    axios
+      .post(baseUrl, resource)
+      .then((response) => setResources([...resources, response.data]))
+  }
+
+  const service = {
+    create,
+  }
+
+  return [resources, service]
+}
+
+const App = () => {
+  const content = useField("text")
+  const name = useField("text")
+  const number = useField("text")
+
+  const [notes, noteService] = useResource("http://localhost:3005/notes")
+  const [persons, personService] = useResource(
+    "http://localhost:3005/persons"
+  )
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    content.reset()
+  }
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value })
+    name.reset()
+    number.reset()
+  }
+
+  const inputParams = params => {
+    const {reset, ...otherParams} = params
+    return otherParams
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...inputParams(content)} />
+        <button>create</button>
+      </form>
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...inputParams(name)} /> <br />
+        number <input {...inputParams(number)} />
+        <button>create</button>
+      </form>
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export default App
