b88e87a1720d9edebf5dc51cd8fc4228af584e19
prb01 <p.bergstroem@gmail.com>
2022-04-23 15:33:19 -0500

    part7c end

--- sandbox/router-app/src/index.js
+++ sandbox/router-app/src/index.js
@@ -1,7 +1,20 @@
 import ReactDOM from "react-dom/client"
 import { useState } from "react"
-import { Table, Form, Button, Alert, Navbar, Nav } from "react-bootstrap"
-
+// import { Table, Form, Button, Alert, Navbar, Nav } from "react-bootstrap"
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  // Button,
+  Alert,
+  AppBar,
+  Toolbar,
+} from "@mui/material"
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,6 +25,36 @@ import {
   useNavigate,
   useMatch,
 } from "react-router-dom"
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div>
@@ -21,12 +64,11 @@ const Home = () => (
       industry. Lorem Ipsum has been the industry's standard dummy text
       ever since the 1500s, when an unknown printer took a galley of type
       and scrambled it to make a type specimen book. It has survived not
-      only five centuries, but also the leap into electroni
-      <Nav className="mr-auto"></Nav>c typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of
-      Letraset sheets containing Lorem Ipsum passages, and more recently
-      with desktop publishing software like Aldus PageMaker including
-      versions of Lorem Ipsum.
+      only five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged. It was popularised in the 1960s with
+      the release of Letraset sheets containing Lorem Ipsum passages, and
+      more recently with desktop publishing software like Aldus PageMaker
+      including versions of Lorem Ipsum.
     </p>
   </div>
 )
@@ -46,18 +88,20 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <Table striped>
-      <tbody>
-        {notes.map((note) => (
-          <tr key={note.id}>
-            <td>
-              <Link to={`/notes/${note.id}`}>{note.content}</Link>
-            </td>
-            <td>{note.user}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map((note) => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>{note.user}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 )
 
@@ -84,19 +128,20 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <Form onSubmit={onSubmit}>
-        <Form.Group>
-          <Form.Label>username:</Form.Label>
-          <Form.Control type="text" name="username" />
-
-          <Form.Label>password:</Form.Label>
-          <Form.Control type="password" name="password" />
-
-          <Button variant="primary" type="submit">
+      <form onSubmit={onSubmit}>
+        <div>
+          username:
+          <Input />
+        </div>
+        <div>
+          <TextField label="password" type="password" />
+        </div>
+        <div>
+          <Button primary="" type="submit">
             login
           </Button>
-        </Form.Group>
-      </Form>
+        </div>
+      </form>
     </div>
   )
 }
@@ -143,57 +188,44 @@ const App = () => {
     ? notes.find((note) => note.id === Number(match.params.id))
     : null
 
-  return (
-    <div className="container">
-      {message && <Alert variant="success">{message}</Alert>}
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/">
-                home
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/notes">
-                notes
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/users">
-                users
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              {user ? (
-                <em style={padding}>{user} logged in</em>
-              ) : (
-                <Link style={padding} to="/login">
-                  login
-                </Link>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
-      <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route
-          path="/users"
-          element={user ? <Users /> : <Navigate replace to="/login" />}
-        />
-        <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <div>
-        <br />
-        <em>Note app, Department of Computer Science 2022</em>
-      </div>
-    </div>
-  )
+ return (
+   <Page>
+     {" "}
+     <Navigation>
+       {" "}
+       <Link style={padding} to="/">
+         home
+       </Link>
+       <Link style={padding} to="/notes">
+         notes
+       </Link>
+       <Link style={padding} to="/users">
+         users
+       </Link>
+       {user ? (
+         <em>{user} logged in</em>
+       ) : (
+         <Link style={padding} to="/login">
+           login
+         </Link>
+       )}
+     </Navigation>
+     <Routes>
+       <Route path="/notes/:id" element={<Note note={note} />} />
+       <Route path="/notes" element={<Notes notes={notes} />} />
+       <Route
+         path="/users"
+         element={user ? <Users /> : <Navigate replace to="/login" />}
+       />
+       <Route path="/login" element={<Login onLogin={login} />} />
+       <Route path="/" element={<Home />} />
+     </Routes>
+     <Footer>
+       {" "}
+       <em>Note app, Department of Computer Science 2022</em>
+     </Footer>{" "}
+   </Page>
+ )
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
