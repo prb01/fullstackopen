1e79f0db3e599ee332ea9ed016af3ebccdb719c4
prb01 <p.bergstroem@gmail.com>
2022-04-29 11:27:16 -0500

    8.7: GraphQL editAuthor

--- part8/graphql/index.js
+++ part8/graphql/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -148,6 +149,16 @@ const resolvers = {
         authors.push({ name: args.author, id: uuidv4() })
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) return null
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) =>
+        a.name === args.name ? updatedAuthor : a
+      )
+      return updatedAuthor
     },
   },
 }
