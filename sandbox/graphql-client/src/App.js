2dd5b62e2c53eab9fb704a3b98a8c3d3d09e2114
prb01 <p.bergstroem@gmail.com>
2022-05-02 11:07:37 -0500

    part8-3

--- sandbox/graphql-client/src/App.js
+++ sandbox/graphql-client/src/App.js
@@ -2,19 +2,29 @@ import { useQuery } from "@apollo/client"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import { ALL_PERSONS } from "./queries"
+import { useState } from "react"
+import Notify from "./components/Notify"
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS, {
-    pollInterval: 60000
+    pollInterval: 60000,
   })
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => setErrorMessage(null), 5000)
+  }
+
   return (
     <div>
-      <PersonForm />
+      <Notify errorMessage={errorMessage} />
+      <PersonForm setError={notify} />
       <Persons persons={result.data.allPersons} />
     </div>
   )
--- sandbox/graphql-client/src/components/PersonForm.js
+++ sandbox/graphql-client/src/components/PersonForm.js
@@ -2,13 +2,18 @@ import { useState } from "react"
 import { useMutation } from "@apollo/client"
 import { ALL_PERSONS, CREATE_PERSON } from "../queries"
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState("")
   const [phone, setPhone] = useState("")
   const [street, setStreet] = useState("")
   const [city, setCity] = useState("")
 
-  const [createPerson] = useMutation(CREATE_PERSON, { refetchQueries: [{ query: ALL_PERSONS}]})
+  const [createPerson] = useMutation(CREATE_PERSON, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    },
+  })
   const submit = (event) => {
     event.preventDefault()
 
