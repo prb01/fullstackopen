77b1da2a91c117f9d2e4c53c8096e987561f4980
prb01 <p.bergstroem@gmail.com>
2022-05-02 10:59:49 -0500

    part8-2

--- sandbox/graphql-client/src/App.js
+++ sandbox/graphql-client/src/App.js
@@ -1,25 +1,23 @@
-import { gql, useQuery } from "@apollo/client"
+import { useQuery } from "@apollo/client"
+import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-    personCount
-  }
-`
+import { ALL_PERSONS } from "./queries"
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS)
+  const result = useQuery(ALL_PERSONS, {
+    pollInterval: 60000
+  })
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
-  return <Persons persons={result.data.allPersons} />
+  return (
+    <div>
+      <PersonForm />
+      <Persons persons={result.data.allPersons} />
+    </div>
+  )
 }
 
 export default App
---
+++ sandbox/graphql-client/src/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client"
+
+export const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+    personCount
+  }
+`
+
+export const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+export const CREATE_PERSON = gql`
+  mutation createPerson(
+    $name: String!
+    $street: String!
+    $city: String!
+    $phone: String
+  ) {
+    addPerson(name: $name, street: $street, city: $city, phone: $phone) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
