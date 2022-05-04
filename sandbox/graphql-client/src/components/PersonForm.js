c0fed3d83147a653be4f000a61028275f7c4775c
prb01 <p.bergstroem@gmail.com>
2022-05-04 08:40:00 -0500

    part8-8

--- sandbox/graphql-client/src/components/PersonForm.js
+++ sandbox/graphql-client/src/components/PersonForm.js
@@ -9,10 +9,18 @@ const PersonForm = ({ setError }) => {
   const [city, setCity] = useState("")
 
   const [createPerson] = useMutation(CREATE_PERSON, {
-    refetchQueries: [{ query: ALL_PERSONS }],
     onError: (error) => {
       setError(error.graphQLErrors[0].message)
     },
+    // refetchQueries: [{ query: ALL_PERSONS }],
+    update: (cache, response) => {
+      cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons, personCount }) => {
+        return {
+          allPersons: allPersons.concat(response.data.addPerson),
+          personCount,
+        }
+      })
+    }
   })
   const submit = (event) => {
     event.preventDefault()
