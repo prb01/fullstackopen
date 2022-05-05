e0cb00398dddac73323053ac4630392eaa5d76fa
prb01 <p.bergstroem@gmail.com>
2022-05-05 18:21:21 -0500

    9.2: typescript exercise calculator

---
+++ part9/typescript-calculators/exerciseCalculator.ts
@@ -0,0 +1,54 @@
+const calculateExercises = (
+  hoursExercised: Array<number>,
+  target: number
+): object => {
+  const periodLength: number = hoursExercised.length;
+  const trainingDays: number = hoursExercised.filter(
+    (hours) => hours > 0
+  ).length;
+  const totalExerciseHours: number = hoursExercised.reduce(
+    (prevValue, curValue) => {
+      return prevValue + curValue;
+    },
+    0
+  );
+  const average: number = totalExerciseHours / periodLength;
+  const diffPercentage: number = (average - target) / target;
+  const success: boolean = average >= target ? true : false;
+  const rating: number =
+    diffPercentage > 0 ? 3 : diffPercentage > -0.3 ? 2 : 1;
+
+  let ratingDescription: string;
+  switch (rating) {
+    case 1:
+      ratingDescription =
+        "god, you're awful.. how do you live with yourself?";
+      break;
+
+    case 2:
+      ratingDescription = "not too bad but could be better";
+      break;
+
+    case 3:
+      ratingDescription = "you. are. amazing! go pat yourself on the back!";
+      break;
+
+    default:
+      ratingDescription = "unknown";
+      break;
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 1));
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 5));
