b4f09965428541df3805bb9171493bd3c7eb2df5
prb01 <p.bergstroem@gmail.com>
2022-05-06 10:03:43 -0500

    9.5: WebBMI

--- part9/typescript-calculators/bmiCalculator.ts
+++ part9/typescript-calculators/bmiCalculator.ts
@@ -1,26 +1,40 @@
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): object => {
+  if (!weight || !height) {
+    throw new Error("malformatted parameters")
+  }
+
   const bmi = weight / ((height / 100) ^ 2);
+  let result = {weight, height}
 
   if (bmi < 18.5) {
-    return "Underweight"
-  }
-  else if (bmi >= 18.5 && bmi < 25) {
-    return "Normal weight"
+    return {
+      ...result,
+      bmi: "Underweight",
+    };
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return {
+      ...result,
+      bmi: "Normal weight",
+    };
   } else if (bmi >= 25 && bmi < 30) {
-    return "Overweight"
+    return {
+      ...result,
+      bmi: "Overweight"
+    };
   } else {
-    return "Obese"
+    return {
+      ...result,
+      bmi: "Obese"
+    };
   }
 };
 
-
-
-// Major adult BMI classifications are 
-// underweight (under 18.5 kg/m2), 
-// normal weight (18.5 to 24.9), 
-// overweight (25 to 29.9), and 
+// Major adult BMI classifications are
+// underweight (under 18.5 kg/m2),
+// normal weight (18.5 to 24.9),
+// overweight (25 to 29.9), and
 // obese (30 or more)
 
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
-console.log(calculateBmi(height, weight));
+// const height: number = Number(process.argv[2])
+// const weight: number = Number(process.argv[3])
+// console.log(calculateBmi(height, weight));
--- part9/typescript-calculators/index.ts
+++ part9/typescript-calculators/index.ts
@@ -1,9 +1,24 @@
 import express = require("express");
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 const port = 3000;
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+  try {
+    res.json(calculateBmi(Number(height), Number(weight)));
+  } catch (error: unknown) {
+    const errorMessage = { error: "" };
+
+    if (error instanceof Error) {
+      errorMessage.error = error.message;
+    }
+    res.json(errorMessage);
+  }
 });
 
 app.listen(port, () => {
