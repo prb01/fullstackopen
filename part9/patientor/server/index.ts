f76c9d6480e31a5f5c05a20b7278e4eab7660d84
prb01 <p.bergstroem@gmail.com>
2022-05-10 10:04:36 -0500

    9.9: Patientor backend step2

--- part9/patientor/server/index.ts
+++ part9/patientor/server/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const PORT = 3001;
 app.use(express.json());
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
 app.get("/api/ping", (_req, res) => {
-  console.log("yo you be pingin me")
-  return res.json("pong")
-})
+  console.log("yo you be pingin me");
+  return res.json("pong");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
