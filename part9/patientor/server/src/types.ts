b468df077e64db9440c3497b924b6775f7e06170
prb01 <p.bergstroem@gmail.com>
2022-05-10 17:09:51 -0500

    9.19: patientor step4

--- part9/patientor/server/src/types.ts
+++ part9/patientor/server/src/types.ts
@@ -11,7 +11,51 @@ export enum Gender {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry;
 
 export interface Patient {
   id: string;
